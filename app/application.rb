b85be290095af8e8b0b40387d16693405158fe15
Donna Amos <donnacamos.developer@gmail.com>
2019-06-25 22:19:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
       search_term = req.params["item"]
       if @@items.include?(search_term) 
         @@cart << search_term 
-        resp.write 
+        resp.write "added"
     else
       resp.write "Path Not Found"
     end
