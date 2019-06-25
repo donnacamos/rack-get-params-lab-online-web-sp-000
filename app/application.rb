f52ee056cb35cbaaf64fc51962a221936c253fa1
Donna Amos <donnacamos.developer@gmail.com>
2019-06-25 22:22:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,19 +17,19 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
-      else 
+      else
         @@cart.each do |item|
-        resp.write "#{item}/n" 
-      end 
-    end 
-    elsif req.path.match(/add/) 
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
       search_term = req.params["item"]
-      if @@items.include?(search_term) 
-        @@cart << search_term 
-        resp.write "added #{search_term}" 
-      else 
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
         resp.write "We don't have that item"
-      end 
+      end
     else
       resp.write "Path Not Found"
     end
