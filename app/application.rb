df16a5b7abdfd781aa5a6c0506b2719bb31128bb
Donna Amos <donnacamos.developer@gmail.com>
2019-06-25 22:18:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,8 @@ class Application
     end 
     elsif req.path.match(/add/) 
       search_term = req.params["item"]
-      if @@items.include?()
+      if @@items.include?(search_term) 
+        
     else
       resp.write "Path Not Found"
     end
